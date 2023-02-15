7ac13b74bedb48256f3f4f96a4dee56775dadf05
Ferdlcz <ferdlcskate@gmail.com>
2023-02-14 23:20:30 -0600

    Episodes start

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const Episodes = () => {
+
+  let [id, setID] = useState(1)
+  let [info, setInfo] = useState([])
+  let {air_date, name} = info
+  console.log(info)
+  var api = `https://rickandmortyapi.com/api/episode/${id}`
+
+  useEffect(() =>{
+
+    (async function () {
+      var data = await fetch(api).then(res=>res.json(0))
+      setInfo(data)
+    })()
+
+  }, [api])
+
   return (
-    <div className='container text-center'><h1>Here will be the episodes where each character appears :)</h1></div>
+    <div className='container'>
+      <div className='row'>
+      <h1 className='text-center mb-4'>
+        Episode : 
+        {" "}
+        <span className='text-primary'>
+        {name === ""? "Unknow" : name}
+        </span>
+      </h1>
+      <h5 className='text-center mb-4'>
+        Air Date :  {" "}
+        <span className='text-primary'>
+        {air_date  === ""? "Unknow" : air_date}
+        </span>
+      </h5>
+      </div>
+      <div className='row '>
+        <div className='col-4'>Pick Episodes</div>
+        <div className='col-8'>Cards are here</div>
+      </div>
+    </div>
   )
 }
